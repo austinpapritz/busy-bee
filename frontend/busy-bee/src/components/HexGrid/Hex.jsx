c71fc85d3d880251a2871fc654eacc2e78341942
Austin Papritz <austin@papritz.dev>
2024-08-17 16:02:01 -0700

    added randomized variation to hex outline

--- frontend/busy-bee/src/components/HexGrid/Hex.jsx
+++ frontend/busy-bee/src/components/HexGrid/Hex.jsx
@@ -1,14 +1,17 @@
+import React from 'react';
 import './HexGrid.css';
 import PropTypes from 'prop-types';
 
 function Hex({ q, r, s, content }) {
-  const size = 50;
+  const size = 50; // half the width of the hexagon
   const width = size * 2;
   const height = Math.sqrt(3) * size;
-  
+
   const x = size * (3 / 2) * q;
   const y = height * (r + q / 2);
-  
+
+  const clipPath = generateRandomHexPolygon();
+
   return (
     <div
       className="hex"
@@ -16,11 +19,36 @@ function Hex({ q, r, s, content }) {
         transform: `translate(${x}px, ${y}px)`,
         width: `${width}px`,
         height: `${height}px`,
+        clipPath: clipPath,
       }}
     >
       {content}
     </div>
   );
+}
+
+function generateRandomHexPolygon() {
+  const basePoints = [
+    [25, 2],  // top-left
+    [75, 2],  // top-right
+    [98, 50], // middle-right
+    [75, 98], // bottom-right
+    [25, 98], // bottom-left
+    [2, 50],  // middle-left
+  ];
+
+  const variation = 2; // percentage variation
+
+  const randomize = (value) => {
+    const offset = Math.random() * variation * 2 - variation; // generates a number between -2 and +2
+    return value + offset;
+  };
+
+  const points = basePoints
+    .map(([x, y]) => `${randomize(x)}% ${randomize(y)}%`)
+    .join(", ");
+
+  return `polygon(${points})`;
 }
 
 // PropTypes validation
