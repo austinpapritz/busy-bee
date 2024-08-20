3b6afe64f970467439044617a3160b7d28de9e68
Austin Papritz <austin@papritz.dev>
2024-08-19 18:32:38 -0700

    added tickHandler and functionality for components to register tick handlers

--- frontend/busy-bee/src/context/TickContext.jsx
+++ frontend/busy-bee/src/context/TickContext.jsx
@@ -1,21 +1,29 @@
-import React, { createContext, useState, useEffect, useContext } from 'react';
+import React, { createContext, useState, useEffect, useContext, useRef } from 'react';
 
 const TickContext = createContext();
 
 export const TickProvider = ({ children, ticksPerSecond = 20 }) => {
   const [tickCount, setTickCount] = useState(0);
   const [gameSpeed, setGameSpeed] = useState(1);
+  const tickHandlers = useRef([]);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTickCount(prevCount => prevCount + gameSpeed);
+      const nextTickCount = tickCount + gameSpeed;
+      setTickCount(nextTickCount);
+
+      tickHandlers.current.forEach(handler => handler(nextTickCount));
     }, 1000 / ticksPerSecond);
 
     return () => clearInterval(interval);
-  }, [ticksPerSecond, gameSpeed]);
+  }, [ticksPerSecond, gameSpeed, tickCount]);
+
+  const registerTickHandler = (handler) => {
+    tickHandlers.current.push(handler);
+  };
 
   return (
-    <TickContext.Provider value={{ tickCount, gameSpeed, setGameSpeed }}>
+    <TickContext.Provider value={{ tickCount, gameSpeed, setGameSpeed, registerTickHandler }}>
       {children}
     </TickContext.Provider>
   );
